5df115a6cc59b18f700b81797ca767b248f6f25f
Jake McCaig <Jake@downparty.io>
2015-12-03 18:56:22 +0000

    Added convert.js to /lib and fahrenheitSim example

--- fahrenheitSim/app.js
+++ fahrenheitSim/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var
-  fahrenheitSim = require('./fahrenheitSim');
+var fahrenheitSim = require('./fahrenheitSim');
 
 fahrenheitSim.simulate(150);
--- fahrenheitSim/fahrenheitSim.js
+++ fahrenheitSim/fahrenheitSim.js
@@ -1,10 +1,11 @@
 'use strict';
 
 var
-  fs = require('fs'),
   util = require('util'),
 
   PIDController = require('./../lib/PIDController'),
+  
+  convert = require('./../lib/convert'),
 
   round;
 
@@ -17,20 +18,19 @@ var
  * @param {Number} temp Point temperature
  */
 exports.simulate = function (temp) {
-  var
+  var                            
+  
     cWater = 4183.2,                                      // Heat capacity J/(kg*°C)
-    m = 40.0,                                             // Weigh of the water (kg)
+    m = convert.gallonsToKg(11),                          // m is looking for the weight of the water kg, use the convert.js lib to be able to enter gallons or quarts instead.
     Ta = 25,                                              // Temperature of the environment (your room, etc.) (°C)
     Rth = 0.024,                                          // Thermal conductivity, total thermal resistance between
                                                           // the water and the surrounding ambient (°C/W)
                                                           // List: http://www.engineeringtoolbox.com/thermal-conductivity-d_429.html
     Pmax = 4000,
-    Tf = true,
 
-    outFile = fs.createWriteStream('./simulator/output.csv'),
     StopTime = 3600,                                      // Total time of the simulation (s)
     deltaT,                                               // Delta temperature
-    T0 = 18,                                              // Start temperature of the water (°C)
+    T0 = Math.round(convert.fToC(90)),                    // Start temperature of the water (°C converted from °F using the convert.js lib then rounded)
     Takt = T0,                                            // Actual temperature of the water (°C)
     dt = 0.25,                                            // Discrete time step (s)
     Pactual,
@@ -41,10 +41,9 @@ exports.simulate = function (temp) {
   // Create a new controller
   controller = new PIDController({
     temp: {
-      ref: temp                                           // Point temperature (This is the temp what you want to reach and hold) (°C)      
+      ref: convert.fToC(temp)                             // Point temperature (This is the temp what you want to reach and hold converted to °C from °F using the convert lib)      
     },
-    Pmax: Pmax,                                           // Max Power, this is the maximum output of your heater (W)
-    Tf: Tf
+    Pmax: Pmax                                            // Max Power, this is the maximum output of your heater (W)
   });
 
   // Simulate
@@ -57,17 +56,13 @@ exports.simulate = function (temp) {
     // Delta Temperature in the k. step
     deltaT = (Pactual - (Takt - Ta) / Rth) * dt / (cWater * m);
     
-    Takt += controller.toFahrenheit(deltaT);
+    Takt += deltaT;
 
     // Write every 30. status into the log file
     if (t % 30 === 0) {
-    //   outFile.write(util.format('%d %d %d %d\n', round(t / 60, 1), round(Takt, 1000), round((Pactual / Pmax) * 100, 1), round(Pactual, 1)));
-        console.log(util.format('%d %d %d %d\n', round(t / 60, 1), round(Takt, 1000), round((Pactual / Pmax) * 100, 1), round(Pactual, 1)));
+        console.log(util.format('Time: %d Actual Temp: %d Output: %d Wattage: %d\n', round(t / 60, 1), convert.cToF(Takt), round((Pactual / Pmax) * 100, 1), round(Pactual, 1)));
     }
   }
-
-  // Close the file stream
-  outFile.end();
 };
 
 
